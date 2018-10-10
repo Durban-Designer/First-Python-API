01f4f74499ee0b1fcb4343eb5c69c4f6976f861e
Royce Birnbaum <royce.birnbaum@gmail.com>
2018-10-09 18:35:15 -0700

    Managed to create the basic user model, add JWT, and set up some endpoints

---
+++ project/server/auth/users.py
@@ -0,0 +1,285 @@
+from flask import Flask, request, Blueprint, make_response, jsonify
+
+from project.server import bcrypt, db
+from project.server.models import User, BlacklistToken
+users = Blueprint('users', __name__, template_folder='templates')
+
+@users.route('/register', methods = ['POST'])
+def register():
+    if request.method == 'POST':
+        # get the post data
+        post_data = request.get_json()
+        # check if user already exists
+        user = User.query.filter_by(email=post_data.get('email')).first()
+        if not user:
+            try:
+                user = User(
+                    email=post_data.get('email'),
+                    password=post_data.get('password')
+                )
+                # insert the user
+                db.session.add(user)
+                db.session.commit()
+                # generate the auth token
+                auth_token = user.encode_auth_token(user.id)
+                responseObject = {
+                    'status': 'success',
+                    'message': 'Successfully registered.',
+                    'auth_token': auth_token.decode()
+                }
+                return make_response(jsonify(responseObject)), 201
+            except Exception as e:
+                responseObject = {
+                    'status': 'fail',
+                    'message': 'Some error occurred. Please try again.'
+                }
+                return make_response(jsonify(responseObject)), 401
+        else:
+            responseObject = {
+                'status': 'fail',
+                'message': 'User already exists. Please Register in.',
+            }
+            return make_response(jsonify(responseObject)), 202
+    else:
+        responseObject = {
+            'status': 'fail',
+            'message': 'This endpoint only accepts Post requests.',
+        }
+        return make_response(jsonify(responseObject)), 401
+
+@users.route('/login', methods = ['POST'])
+def login():
+    if request.method == 'POST':
+        # get the post data
+        post_data = request.get_json()
+        try:
+            # fetch the user data
+            user = User.query.filter_by(
+                email=post_data.get('email')
+            ).first()
+            if user and bcrypt.check_password_hash(
+                user.password, post_data.get('password')
+            ):
+                auth_token = user.encode_auth_token(user.id)
+                if auth_token:
+                    responseObject = {
+                        'status': 'success',
+                        'message': 'Successfully logged in.',
+                        'auth_token': auth_token.decode()
+                    }
+                    return make_response(jsonify(responseObject)), 200
+            else:
+                responseObject = {
+                    'status': 'fail',
+                    'message': 'User does not exist.'
+                }
+                return make_response(jsonify(responseObject)), 404
+        except Exception as e:
+            print(e)
+            responseObject = {
+                'status': 'fail',
+                'message': 'Try again'
+            }
+            return make_response(jsonify(responseObject)), 500
+    else:
+        responseObject = {
+            'status': 'fail',
+            'message': 'This endpoint only accepts Post requests.',
+        }
+        return make_response(jsonify(responseObject)), 401
+
+
+@users.route('/get', methods = ['GET'])
+def get():
+    if request.message == 'GET':
+        # get the auth token
+        auth_header = request.headers.get('Authorization')
+        if auth_header:
+            try:
+                auth_token = auth_header.split(" ")[1]
+            except IndexError:
+                responseObject = {
+                    'status': 'fail',
+                    'message': 'Bearer token malformed.'
+                }
+                return make_response(jsonify(responseObject)), 401
+        else:
+            auth_token = ''
+        if auth_token:
+            resp = User.decode_auth_token(auth_token)
+            if not isinstance(resp, str):
+                user = User.query.filter_by(id=resp).first()
+                responseObject = {
+                    'status': 'success',
+                    'data': {
+                        'user_id': user.id,
+                        'email': user.email,
+                        'admin': user.admin,
+                        'registered_on': user.registered_on
+                    }
+                }
+                return make_response(jsonify(responseObject)), 200
+            responseObject = {
+                'status': 'fail',
+                'message': resp
+            }
+            return make_response(jsonify(responseObject)), 401
+        else:
+            responseObject = {
+                'status': 'fail',
+                'message': 'Provide a valid auth token.'
+            }
+            return make_response(jsonify(responseObject)), 401
+    else:
+        responseObject = {
+            'status': 'fail',
+            'message': 'This endpoint only accepts Get requests.',
+        }
+        return make_response(jsonify(responseObject)), 401
+
+@users.route('/update', methods = ['PUT'])
+def update():
+    if request.method == 'PUT':
+        # get the put data
+        put_data = request.get_json()
+        # get the auth token
+        auth_header = request.headers.get('Authorization')
+        if auth_header:
+            try:
+                auth_token = auth_header.split(" ")[1]
+            except IndexError:
+                responseObject = {
+                    'status': 'fail',
+                    'message': 'Bearer token malformed.'
+                }
+                return make_response(jsonify(responseObject)), 401
+        else:
+            auth_token = ''
+        if auth_token:
+            resp = User.decode_auth_token(auth_token)
+            if not isinstance(resp, str):
+                user = User.query.filter_by(id=resp).first()
+                user.email = post_data.get('email')
+                user.password = post_data.get('password')
+                db.session.commit()
+                responseObject = {
+                    'status': 'success',
+                    'data': {
+                        'user_id': user.id,
+                        'email': user.email,
+                        'admin': user.admin,
+                        'registered_on': user.registered_on
+                    }
+                }
+                return make_response(jsonify(responseObject)), 200
+            responseObject = {
+                'status': 'fail',
+                'message': resp
+            }
+            return make_response(jsonify(responseObject)), 401
+        else:
+            responseObject = {
+                'status': 'fail',
+                'message': 'Provide a valid auth token.'
+            }
+            return make_response(jsonify(responseObject)), 401
+    else:
+        responseObject = {
+            'status': 'fail',
+            'message': 'This endpoint only accepts Put requests.',
+        }
+        return make_response(jsonify(responseObject)), 401
+
+@users.route('/delete', methods = ['DELETE'])
+def delete():
+    if request.method == 'DELETE':
+        # get the put data
+        put_data = request.get_json()
+        # get the auth token
+        auth_header = request.headers.get('Authorization')
+        if auth_header:
+            try:
+                auth_token = auth_header.split(" ")[1]
+            except IndexError:
+                responseObject = {
+                    'status': 'fail',
+                    'message': 'Bearer token malformed.'
+                }
+                return make_response(jsonify(responseObject)), 401
+        else:
+            auth_token = ''
+        if auth_token:
+            resp = User.decode_auth_token(auth_token)
+            if not isinstance(resp, str):
+                user = User.query.filter_by(id=resp).first()
+                db.session.delete(user)
+                db.session.commit()
+                responseObject = {
+                    'status': 'success'
+                }
+                return make_response(jsonify(responseObject)), 200
+            responseObject = {
+                'status': 'fail',
+                'message': resp
+            }
+            return make_response(jsonify(responseObject)), 401
+        else:
+            responseObject = {
+                'status': 'fail',
+                'message': 'Provide a valid auth token.'
+            }
+            return make_response(jsonify(responseObject)), 401
+    else:
+        responseObject = {
+            'status': 'fail',
+            'message': 'This endpoint only accepts Delete requests.',
+        }
+        return make_response(jsonify(responseObject)), 401
+
+@users.route('/logout', methods = ['POST'])
+def logout():
+    if request.methods == 'POST':
+        # get auth token
+        auth_header = request.headers.get('Authorization')
+        if auth_header:
+            auth_token = auth_header.split(" ")[1]
+        else:
+            auth_token = ''
+        if auth_token:
+            resp = User.decode_auth_token(auth_token)
+            if not isinstance(resp, str):
+                # mark the token as blacklisted
+                blacklist_token = BlacklistToken(token=auth_token)
+                try:
+                    # insert the token
+                    db.session.add(blacklist_token)
+                    db.session.commit()
+                    responseObject = {
+                        'status': 'success',
+                        'message': 'Successfully logged out.'
+                    }
+                    return make_response(jsonify(responseObject)), 200
+                except Exception as e:
+                    responseObject = {
+                        'status': 'fail',
+                        'message': e
+                    }
+                    return make_response(jsonify(responseObject)), 200
+            else:
+                responseObject = {
+                    'status': 'fail',
+                    'message': resp
+                }
+                return make_response(jsonify(responseObject)), 401
+        else:
+            responseObject = {
+                'status': 'fail',
+                'message': 'Provide a valid auth token.'
+            }
+            return make_response(jsonify(responseObject)), 403
+    else:
+        responseObject = {
+            'status': 'fail',
+            'message': 'This endpoint only accepts Post requests.',
+        }
+        return make_response(jsonify(responseObject)), 401
---
+++ project/server/config.py
@@ -0,0 +1,25 @@
+# project/server/config.py
+
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+postgres_local_base = 'postgresql:///'
+database_name = 'flask_jwt_auth'
+
+
+class BaseConfig:
+    SECRET_KEY = os.getenv('SECRET_KEY', 'TheKeyToDataYes')
+    DEBUG = False
+    BCRYPT_LOG_ROUNDS = 13
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+class DevelopmentConfig(BaseConfig):
+    DEBUG = True
+    BCRYPT_LOG_ROUNDS = 4
+    SQLALCHEMY_DATABASE_URI = postgres_local_base + database_name
+
+
+class ProductionConfig(BaseConfig):
+    SECRET_KEY = 'TheKeyToDataYes'
+    DEBUG = False
+    SQLALCHEMY_DATABASE_URI = 'postgresql:///example'
